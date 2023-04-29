7980f40d9d594b64a025018ee7995aad76aa429e
Xavier Chalut <xavier.chalut@gmail.com>
2023-04-29 17:27:00 +0200

    Utilisation des nodes Planete pour la creation de l'univers

--- scripts/univers_generator.gd
+++ scripts/univers_generator.gd
@@ -1,26 +1,33 @@
 extends Node3D
 class_name Univers
 
+var planeteNode = preload("res://nodes/planete.tscn")
+
+#TODO : Expose variables
 var seed : int = 0
 var radius : int = 15 
-var planetesNumber : int = 20
+var tinyPlanetesNumber : int = 15
+var mediumPlanetesNumber : int = 5
+var bigPlanetesNumber : int = 2
 var interplanetaryMinDistance: int = 10
 var rng = RandomNumberGenerator.new()
 #TODO: array of "Planet" objects, for now use Vector3 to save positions
-var planeteArray: Array[Vector3]
+var planeteArray: Array[Node3D]
 
 func _init() -> void:
 	rng.set_seed(seed)
-	# Generate planeteArray
-	for planeteIndex in planetesNumber:
-		planeteArray.push_back(generateValidPosition())
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for planeteIndex in planetesNumber:
-		createShere(planeteArray[planeteIndex])
+	# Feed the planeteArray
+	for planeteIndex in bigPlanetesNumber:
+		createBigPlanete(generateValidPosition())
+	for planeteIndex in mediumPlanetesNumber:
+		createMediumPlanete(generateValidPosition())
+	for planeteIndex in tinyPlanetesNumber:
+		createTinyPlanete(generateValidPosition())
 
-
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
@@ -37,9 +44,30 @@ func generateValidPosition() -> Vector3:
 func isValidPosition(pPosition : Vector3) -> bool:
 	# TODO : change interplanetaryMinDistance according to the size of the planet to generate
 	for planete in planeteArray:
-		if pPosition.distance_to(planete) < interplanetaryMinDistance:
+		if pPosition.distance_to(planete.position) < interplanetaryMinDistance:
 			return false
 	return true
+	
+func createTinyPlanete(pPosition : Vector3) -> void : 
+	var planeteInstance = planeteNode.instantiate()
+	planeteInstance.radius = 1
+	add_child(planeteInstance)
+	planeteInstance.set_position(pPosition)
+	planeteArray.push_back(planeteInstance)
+	
+func createMediumPlanete(pPosition : Vector3) -> void : 
+	var planeteInstance = planeteNode.instantiate()
+	planeteInstance.radius = 3
+	add_child(planeteInstance)
+	planeteInstance.set_position(pPosition)
+	planeteArray.push_back(planeteInstance)
+	
+func createBigPlanete(pPosition : Vector3) -> void : 
+	var planeteInstance = planeteNode.instantiate()
+	planeteInstance.radius = 6
+	add_child(planeteInstance)
+	planeteInstance.set_position(pPosition)
+	planeteArray.push_back(planeteInstance)
 	
 # DEBUG FUNCTION TO GENERATE SPHERE
 func createShere(pPosition : Vector3) -> void :  
