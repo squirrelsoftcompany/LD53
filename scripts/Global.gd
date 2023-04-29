8a116059a8a3d220d45ec78339243a05986d0e14
Jean-François Erdelyi <jferdelyi@gmail.com>
2023-04-29 13:55:23 +0200

    [QA] Remove comment

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -30,14 +30,8 @@ func goto_main_menu() -> void:
 
 
 func _goto_scene(scene: Resource) -> void:
-	# This function will usually be called from a signal callback,
-	# or some other function in the current scene.
-	# Deleting the current scene at this point is
-	# a bad idea, because it may still be executing code.
-	# This will result in a crash or unexpected behavior.
-	
-	# The solution is to defer the load to a later time, when
-	# we can be sure that no code from the current scene is running:
+	# Defer the load to a later time, when
+	# we can be sure that no code from the current scene is running
 	call_deferred("_deferred_goto_scene", scene)
 
 
