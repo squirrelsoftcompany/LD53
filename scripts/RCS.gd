2a6665eb9f1070a8b5ccdd3d93f4008c63e84ef3
ChristopheClaustre <christophe.claustre.31@gmail.com>
2023-05-01 16:48:21 +0200

    Let me present you now the RCS

--- scripts/RCS.gd
+++ scripts/RCS.gd
@@ -1,24 +1,17 @@
 extends Node3D
 
-var initialThrust 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+
+func apply_raw_input(raw_input : Vector3):
+	$RCS_left.emitting  = raw_input.x < -0.2
+	$RCS_right.emitting = raw_input.x >  0.2
+	$RCS_back.emitting  = raw_input.z < -0.2
+	$RCS_back2.emitting = raw_input.z < -0.2
+	$RCS_down.emitting  = raw_input.y < -0.2
+	$RCS_down2.emitting = raw_input.y < -0.2
+	$RCS_up.emitting    = raw_input.y >  0.2
+	$RCS_up2.emitting   = raw_input.y >  0.2
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	var move = Vector3(0,0,0)
-	move.z = (Input.get_action_strength("go_back"))
-	move.z += -1*(Input.get_action_strength("go_front"))
-	move.x += -1*(Input.get_action_strength("go_left"))
-	move.x += (Input.get_action_strength("go_right"))
-	move.y += Input.get_action_strength("go_down")
-	move.y += -1*(Input.get_action_strength("go_up"))
-	$"../../Load/engines".position.z = move.z/5
-	if not move.distance_to(Vector3(0,0,0)):
-		move = to_global(move)
-	$RCS.process_material.set("shader_parameter/forceX",move.x)
-	$RCS.process_material.set("shader_parameter/forceY",move.y)
-	$RCS.process_material.set("shader_parameter/forceZ",move.z)
-	pass
+func apply_jump():
+	$RCS_jump.emitting = true
+	$RCS_jump2.emitting = true
