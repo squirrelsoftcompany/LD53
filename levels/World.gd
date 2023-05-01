1e8f3804a51dbbc104caf0476aeb115c5079a1e1
Jean-François Erdelyi <jferdelyi@gmail.com>
2023-05-01 22:26:31 +0200

    Black magic delivery time computing

--- levels/World.gd
+++ levels/World.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-var _current_delivery_time := 0
+var _current_delivery_time := 30.0
 
 @onready var _hud := $HUD
 @onready var _fail_audio_stream := $FailAudioStream
@@ -12,22 +12,18 @@ var _current_delivery_time := 0
 
 
 func _ready() -> void:
-	# Init TODO
-	Global.delivery_count = 1
-	Global.delivery_total = 10
-	_current_delivery_time = compute_current_delivery_time()
-	Global.delivery_time = _current_delivery_time
-	Global.global_time = 90.0
+	Global.connect("new_delivery_point", _on_new_delivery_point)
 	Global.connect("gameover_timeout", _on_gameover_timeout)
 	Global.connect("delivery_timeout", _on_delivery_timeout)
 	Global.connect("delivery_ok", _on_delivery_ok)
 	Global.connect("gameover_dead", _on_gameover_dead)
+	Global.delivery_count = 1
+	Global.delivery_total = 10
+	Global.delivery_time = 30
+	Global.global_time = 90.0
 
 
 func _process(delta: float) -> void:
-	if _current_delivery_time == -1:
-		_current_delivery_time = compute_current_delivery_time()
-		Global.delivery_time = _current_delivery_time
 	Global.delivery_time = Global.delivery_time - delta
 	Global.global_time = Global.global_time - delta
 	if Global.global_time <= 12 or Global.delivery_time <= 12:
@@ -48,7 +44,7 @@ func _on_gameover_timeout() -> void:
 
 func _on_delivery_timeout() -> void:
 	_fail_audio_stream.play()
-	Global.delivery_time = 30.0
+	Global.delivery_time = _current_delivery_time
 	if Global.delivery_count == Global.delivery_total:
 		_victory_audio_stream.play()
 		_hud.gameover("Congratulation: All deliveries are completed for today")
@@ -58,7 +54,6 @@ func _on_delivery_timeout() -> void:
 func _on_delivery_ok() -> void:
 	_delivery_ok_audio_stream.play()
 	Global.points += _current_delivery_time - Global.delivery_time
-	_current_delivery_time = compute_current_delivery_time()
 	Global.delivery_time = _current_delivery_time
 	_timer_audio_stream.stop()
 	Global.global_time += 15
@@ -68,9 +63,12 @@ func _on_delivery_ok() -> void:
 	Global.delivery_count += 1
 
 
-func compute_current_delivery_time() -> float:
-	var caracter :Node3D= _univers.get_caracter()
-	var current_delivery_point := Global.current_delivery_point
-	if current_delivery_point == null:
-		return -1.0
-	return caracter.position.distance_to(current_delivery_point.position)
+func _on_new_delivery_point(delivery_point: Node3D) -> void:
+	# Init Todo
+	_current_delivery_time = compute_current_delivery_time(delivery_point)
+	Global.delivery_time = _current_delivery_time
+
+
+func compute_current_delivery_time(delivery_point: Node3D) -> float:
+	var caracter:Node3D = _univers.get_caracter()
+	return sqrt(delivery_point.global_position.distance_to(caracter.global_position)) * 2.5
